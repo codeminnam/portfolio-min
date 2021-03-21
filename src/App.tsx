f86528bd76f9e00a0e37f43e759b01eb7bc13ed8
codeminnam <code.minnam@gmail.com>
2021-03-21 20:42:49 +0900

    feat: add component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import About from './components/About';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
-function App() {
-  return <div className="App">Hello World!</div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
