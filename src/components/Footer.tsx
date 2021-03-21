116f98eb6f0dbe4985cedf58e2252d7d2d08344a
codeminnam <code.minnam@gmail.com>
2021-03-22 02:32:08 +0900

    feat: add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faLinkedinIn, faGithub);
+
+const FooterContainer = styled.footer`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 20px 40px;
+  background-color: #f5f5f7;
+  text-align: center;
+`;
+
+const FooterText = styled.p`
+  color: #86868b;
+  font-size: 12px;
+`;
+
+const IconContainer = styled.div`
+  margin-top: 5px;
+`;
+
+const Link = styled.a`
+  color: #515154;
+  &:hover {
+    cursor: pointer;
+  }
+  & + & {
+    margin-left: 5px;
+  }
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer>
+      <FooterText>Copyright © 2021 Nam Hyunmyung</FooterText>
+      <IconContainer>
+        <Link href="https://www.linkedin.com/in/hyunmyung-nam/">
+          <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+        </Link>
+        <Link href="https://github.com/codeminnam">
+          <FontAwesomeIcon icon={['fab', 'github']} />
+        </Link>
+      </IconContainer>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
