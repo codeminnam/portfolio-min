648406b9ffd4018f1d8c7ecbc70afd74cc20eeed
codeminnam <code.minnam@gmail.com>
2021-03-23 01:04:50 +0900

    feat: add portfolio section

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PortfolioSection = styled.section`
-  background-color: #ffffff;
+  background-color: #f2f8fa;
 `;
 
 const PortfolioContainer = styled.div`
@@ -20,7 +20,8 @@ const PortfolioTitle = styled.strong`
   margin-bottom: 10px;
 `;
 
-const PortfolioDesc = styled.div``;
+const PortfolioItem = styled.div``;
+const ItemTitle = styled.div``;
 
 const Portfolio = () => {
   return (
@@ -28,13 +29,61 @@ const Portfolio = () => {
       <PortfolioContainer>
         <PortfolioContent>
           <PortfolioTitle>Portfolio</PortfolioTitle>
-          <PortfolioDesc>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
-            ratione omnis reiciendis dolore maxime distinctio, explicabo
-            recusandae, perferendis deserunt fugiat repudiandae exercitationem
-            iste. Fugit nobis, culpa praesentium dignissimos accusantium
-            sapiente.
-          </PortfolioDesc>
+          <PortfolioItem>
+            <ItemTitle>TS-Booklist</ItemTitle>
+            <p>A website you can manage book information fetched from API.</p>
+            <p>
+              Implemented CRUD in module which contains components, action
+              creators, reducer, and saga. Divided and implemented components
+              for their own purposes; pages(routing), containers(data
+              processing), and components(rendering DOM).
+            </p>
+            <p>
+              ReactJs, Typescript, Redux-saga, REST API, Antd, React Hooks, ES6
+            </p>
+          </PortfolioItem>
+          <PortfolioItem>
+            <ItemTitle>Reddit Music Player</ItemTitle>
+            <p>
+              A music player website fetching genre information from reddit and
+              making a playlist.
+            </p>
+            <p>
+              Implemented interaction between genre list, playlist, and youtube
+              video player components, with data fetched via graphQL. Mixed
+              class and functional components.
+            </p>
+            <p>ReactJs, GraphQL, ES6</p>
+          </PortfolioItem>
+          <PortfolioItem>
+            <ItemTitle>HonyakunHonyakun</ItemTitle>
+            <p>
+              An individual OJT project created from scratch: from defining
+              requirements through front-end, back-end, and DB development to
+              deployment.
+            </p>
+            <p>
+              Wrote requirement documents, designed and created DB, and made MVC
+              and Rest controller, with functions including user management,
+              dashboard, board, and comment.
+            </p>
+            <p>Javascript, Java, Spring Boot, REST API, Thymeleaf, MariaDB</p>
+          </PortfolioItem>
+          <PortfolioItem>
+            <ItemTitle>Redux Comment</ItemTitle>
+            <p>
+              Comment application in which comments are fetched via API with
+              pagination.
+            </p>
+            <p>
+              Implemented CRUD that interacts with API server. 3 components -
+              comment list, pagination, and form share same state.
+            </p>
+            <p>
+              ReactJs, Redux-thunk, REST API, Styled-components, React Hooks,
+              ES6, JSON-server
+            </p>
+          </PortfolioItem>
         </PortfolioContent>
       </PortfolioContainer>
     </PortfolioSection>
